9ba21bdcad295203f49470300e0542884731f757
Sarvenaz Mohammadi <sarv.mr@gmail.com>
2022-03-21 15:14:40 -0700

    resolved reading user_name and daily quotes issue

--- scripts/home.js
+++ scripts/home.js
@@ -1,33 +1,38 @@
-function insertName() {
-    // to check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid); // let me know who is the user that logged in the uid
-            currentUser = db.collection("users").doc(user.uid); // will do to the firestone and 
-            currentUser.get().then(doc => {
-                //get the user name
-                var userName = doc.data();
-                console.log(userName);
-                $("#name-goes-here").text(userName) //jquery
-
-
-            })
-        }
-
-
-    })
-}
-insertName();
-
 function read_display_Quote() {
-    db.collection("quotes").doc("tuesday")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
-      .onSnapshot(tuesdayDoc => {                                                               //arrow notation
-           console.log("current document data: " + tuesdayDoc.data());                          //.data() returns data object
-           document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().quote;      //using javascript to display the data on the right place
+    var event = new Date(); 
+    var options = { weekday: 'long' }; 
+    var weekDay = event. toLocaleDateString('en-US', options).toLowerCase();
+    console. log(weekDay);
+    
+    db.collection("quotes").doc(weekDay)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(weekdayDoc => {                                                               //arrow notation
+           console.log(weekdayDoc.data());                          //.data() returns data object
+           document.getElementById("quote-goes-here").innerText = weekdayDoc.data().quotes;      //using javascript to display the data on the right place
            
+
            //Here are other ways to access key:value data fields
            //$('#quote-goes-here').text(c.data().quote);                                       //using jquery object dot notation
            //$("#quote-goes-here").text(c.data()["quote"]);                                    //using json object indexing
       })
 }
 read_display_Quote()        //calling the function
+
+function insertName() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // let me know who is the user that logged in the uid
+            currentUser = db.collection("users").doc(user.uid); // will do to the firestone and 
+            currentUser.get().then(userDoc=>{
+                //get the user name
+                var user_Name = userDoc.data().name;                
+                console.log(user_Name);
+                document.getElementById("name-goes-here").innerHTML=user_Name;
+            })
+            .finally();
+        }
+
+
+    })
+}
+insertName();
