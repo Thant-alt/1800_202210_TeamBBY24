8680dd56d6967e17b68205c1a10ada055022d29f
Tushar Gandhi <gandhitushar702@gmail.com>
2022-03-25 20:43:36 -0700

    Tushar has done About.html

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -35,7 +35,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'home.html',
+    signInSuccessUrl: 'editProfile.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
