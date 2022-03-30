e5fad15defadcb0cac3412128f109e8ab46d2812
Chi Lan <chilanhuynh@hotmail.com>
2022-03-30 12:15:47 -0700

    rating.html and rating.js

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,15 +1,43 @@
-function writeNewComment(uid, username, picture, title, body) {
-    const db = getDatabase();
+function saveRating() {
+    var foodType = document.getElementById('food_type').value;
+    var restComment = document.getElementById('rest_comment').value;
 
-    const postData = {
-        author: username,
-        uid: uid,
-        body: body,
-        title: title
-    }
+    if ('input[id="1"]:checked') {
+        var starRating = document.querySelector("#1").value;
+    } 
+    
+    if ('input[id="2"]:checked') {
+        var starRating = document.querySelector("#2").value;
+    } 
 
-    firebase.database().ref("Comments").set ({
-        comment: document.getElementById("comment").value
-    });
+    if ('input[id="3"]:checked') {
+        var starRating = document.querySelector("#3").value;
+    } 
+
+    if ('input[id="4"]:checked') {
+        var starRating = document.querySelector("#4").value;
+    } 
+    
+    if ('input[id="4"]:checked') {
+        var starRating = document.querySelector("#5").value;
+    } 
+
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            db.collection("rating").add({         //write to firestore. We are using the UID for the ID in users collection
+                userID: user.uid,
+                type: foodType,
+                comment: restComment,
+                rating: starRating,
+            }).then(function () {
+                console.log("New rating added to firestore");
+                window.location.assign("rating-thankyou.html");        //re-direct to main.html after signup
+            })
+                .catch(function (error) {
+                    console.log("Error adding new user: " + error);
+                });
+
+        }
+    })
 }
-writeData();
