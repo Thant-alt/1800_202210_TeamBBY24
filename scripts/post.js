399a71a22b7c8237adfc7b30883b6b6bfc024374
Sarvenaz Mohammadi <sarv.mr@gmail.com>
2022-03-29 23:35:55 -0700

    page post.js and post.html works fully and data will be saved to firebase and firestorage succesfully

--- scripts/post.js
+++ scripts/post.js
@@ -1,38 +1,61 @@
+function uploadedPicture() {
 
-function showUploadedPicture(){
-    const image_input = document.querySelector("#image_input");
-    const image = document.getElementById("mypic-goes-here");
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+        const image_input = document.querySelector("#image_input");
+        var image = document.getElementById("mypic-goes-here");
 
-    image_input.addEventListener("change" , function(e){
-        console.log(image_input.value);
-        
-        //the change event returns a file "e.target.files[0]"
-        var blob = URL.createObjectURL(e.target.files[0]);
+        image_input.addEventListener("change", function (e) {
+            console.log(image_input.value);
 
-        //change the DOM img element source to point to this file
-        image.src = blob;    //assign the "src" property of the "img" tag
-    })
+            //the change event returns a file "e.target.files[0]"
+            var blob = URL.createObjectURL(e.target.files[0]);
+
+            //change the DOM img element source to point to this file
+            image.src = blob;    //assign the "src" property of the "img" tag
+
+            var storageRef = firebase.storage().ref("images/" + user.uid + ".jpg"); // Get reference
+            // Upload picked file to cloud storage
+            storageRef.put(e.target.files[0])
+                .then(function () {
+                    storageRef.getDownloadURL()
+                        .then(function (url) { // Get URL of the uploaded file
+                            console.log(url); // Save the URL into users collection
+                            // db.collection("restaurant").doc(user.uid).update({
+                            //     "restaurant-pic": url
+                            // })
+                                // .then(function () {
+                                //     console.log('Added Pic URL to Firestore.');
+                                // })
+                            image.dataset.url=url;
+                        })
+                })
+        })
+    } else {
+        console.log("No user is singned in");
+        window.location.href = "login.html";
+    }
+});
 }
-showUploadedPicture();
+uploadedPicture();
 
 function editRestInfo() {
     //Enable the form fields
     document.getElementById('restaurantInfoFields').disabled = false;
- }
- 
- editRestInfo();
+}
+
+editRestInfo();
 
 
-
- function saveRestaurantInfo() {
-    var restaurantName = document.getElementById('rest_name').value;       
-    var foodType = document.getElementById('food_type').value;     
+function saveRestaurantInfo() {
+    var restaurantName = document.getElementById('rest_name').value;
+    var foodType = document.getElementById('food_type').value;
     var restComment = document.getElementById('rest_comment').value;
     var customerService = document.querySelector('input[name="customer-service"]:checked').value;
     var priceRange = document.querySelector('input[name="price-range"]:checked').value;
+    var imgsrc = document.getElementById("mypic-goes-here").dataset.url;
 
-    
-    
+
 
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
@@ -43,39 +66,19 @@ function editRestInfo() {
                 type: foodType,
                 comment: restComment,
                 custom: customerService,
-                price: priceRange
+                price: priceRange,
+                restaurantURL: imgsrc
             }).then(function () {
                 console.log("New restaurant added to firestore");
                 window.location.assign("main.html");        //re-direct to main.html after signup
             })
-            .catch(function (error) {
-                console.log("Error adding new user: " + error);
-            });
- 
+                .catch(function (error) {
+                    console.log("Error adding new user: " + error);
+                });
+
         }
     })
 }
 
-// // Create a root reference
-// var storageRef = db.ref();
 
-// // Create a reference to 'mountains.jpg'
-// var postRef = storageRef.child('BBY03.jpg');
 
-// // Create a reference to 'images/mountains.jpg'
-// var postImagesRef = storageRef.child('images/BBY03.jpg');
-
-// // While the file names are the same, the references point to different files
-// postRef.name === postImagesRef.name;           // true
-// postRef.fullPath === postImagesRef.fullPath;   // false 
-
-// function uploadBlob(file) {
-//     const ref = firebase.storage().ref().child('BBY03.jpg');
-  
-//     // [START storage_upload_blob]
-//     // 'file' comes from the Blob or File API
-//     ref.put(file).then((snapshot) => {
-//       console.log('Uploaded a blob or file!');
-//     });
-//     // [END storage_upload_blob]
-//   }
