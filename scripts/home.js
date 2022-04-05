db9e4707e64e1581eb26306b84e47acd4823b621
Sarvenaz Mohammadi <sarv.mr@gmail.com>
2022-04-04 23:28:10 -0700

    Profile page now have the function to upload and save the profile picture to firestore. Logout function works properly now. minor changes to UI in main page

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,67 @@
+firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+         console.log("No user is singned in");
+         window.location.href = "login.html";
+    }
+});
+
+function uploadProfilePicture() {
+    console.log("Derp");
+
+
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+        const image_input2 = document.querySelector("#image_input");
+        console.log(image_input2);
+        var image = document.getElementById("mypic-goes-here");
+
+        db.collection("users").doc(user.uid).get().then(
+            retrievedUser => {
+                var profilePicture = retrievedUser.data().profilePicture;
+                if (profilePicture){
+                    image.src = profilePicture;
+                }
+            }
+        )
+
+        image_input2.addEventListener("change", function (e) {
+            console.log(image_input2.value);
+
+            //the change event returns a file "e.target.files[0]"
+            if (e.target.files[0]){
+                var blob = URL.createObjectURL(e.target.files[0]);
+
+                //change the DOM img element source to point to this file
+                image.src = blob;    //assign the "src" property of the "img" tag
+    
+                var storageRef = firebase.storage().ref("images/" + new Date().getTime() + ".jpg"); // Get reference
+                // Upload picked file to cloud storage
+                storageRef.put(e.target.files[0])
+                    .then(function () {
+                        storageRef.getDownloadURL()
+                            .then(function (url) { // Get URL of the uploaded file
+                                console.log(url); // Save the URL into users collection
+                                db.collection("users").doc(user.uid).update({
+                                    "profilePicture": url
+                                })
+                                    .then(function () {
+                                        console.log('Added Pic URL to Firestore.');
+                                    })
+                            })
+                    })
+            }
+            
+        })
+    } else {
+        console.log("No user is singned in");
+        window.location.href = "login.html";
+    }
+});
+}
+uploadProfilePicture();
+
+
 function read_display_Quote() {
     var event = new Date(); 
     var options = { weekday: 'long' }; 
@@ -36,3 +100,20 @@ function insertName() {
     })
 }
 insertName();
+
+function logOut(){
+    console.log("Sup");
+
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            firebase.auth().signOut().then(() => {
+                // Sign-out successful.
+              }).catch((error) => {
+                  console.log(error);
+                // An error happened.
+              });
+        }
+    })
+    window.location.assign("index.html");
+}
